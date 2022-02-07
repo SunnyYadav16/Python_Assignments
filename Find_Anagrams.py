30983e552415565ef62425beeda500b201007a05
Sunny Yadav <180120107188@git.org.in>
2022-02-07 20:38:28 +0530

    Final optimized code for finding Anagrams

--- Find_Anagrams.py
+++ Find_Anagrams.py
@@ -1,12 +1,14 @@
 # Program to find the anagrams of words
 
 # A function to find the anagrams of the input string
-def Anagram(my_list):
+def find_anagram(my_list):
     my_dict = {}
 
     for word in my_list:
+        # sorting the words from the list and using them as keys of the other same words
         my_key = ''.join(sorted(word))
 
+        # checking if the key is already present in the dictionary, appending it if not present.
         if my_key in my_dict.keys():
             my_dict[my_key].append(word)
         else:
@@ -18,16 +20,17 @@ def Anagram(my_list):
         result.append(value)
     return result
 
+
 if __name__ == "__main__":
 
     try:
         my_input_list = input().split(',')
 
-        # If input string is empty
+        # Checking if input string is empty
         if len(my_input_list) == 1 and "" in my_input_list:
             print([""])
         else:
-            final_output = Anagram(my_input_list)
+            final_output = find_anagram(my_input_list)
             print(final_output)
 
     except KeyError:
